c00eabf87f3ff826e62d527418260000e96cf1c5
askar <bolotbekov06@gmail.com>
2016-10-11 14:09:49 +0600

    added test_settings.py

--- univer/settings.py
+++ univer/settings.py
@@ -26,10 +26,6 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'web_application',
-    'django_jenkins',
-    'aloe_django',
-    'django_nose',
-    'acceptance_tests',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -71,7 +67,7 @@ WSGI_APPLICATION = 'univer.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'test_db.sqlite3'),
     }
 }
 
