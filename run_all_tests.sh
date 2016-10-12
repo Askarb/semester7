42be99bb2c21acf00afaafd671425e7d1aa06c80
askar <bolotbekov06@gmail.com>
2016-10-13 05:30:47 +0600

    Changed run_al_test.sh to use new settings file

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -3,5 +3,5 @@ echo '**************************************Run unittest************************
 ./manage.py test quadratic/
 echo '*********************************Unittest has finished***********************************'
 echo '**********************************Run acceptance tests***********************************'
-./manage.py harvest --liveserver localhost:8001 -v 3 --noinput
+./manage.py harvest --settings=univer.test_settings --liveserver localhost:8001 -v 3 --noinput
 echo '******************************Acceptance tests has finished******************************'
